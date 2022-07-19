4a31f3c794ae5c881449b90a35684aa4d5d3fd3f
qklee <qkli@robvision.cn>
2022-07-19 22:48:25 +0800

    init

---
+++ base/src/com/noslime/base/grammer/SayHiToFuture.java
@@ -0,0 +1,15 @@
+package com.noslime.base.grammer;
+
+/**
+ * @author qklee
+ * @description
+ * @date 2022/7/19
+ * @since v.1.0
+ */
+public class SayHiToFuture {
+
+    public static void main(String[] args) {
+        System.out.println("Hi, future!");
+    }
+
+}
